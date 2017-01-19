c7140dae56a02b0cb6a3e148e3363096f2ac1589
Bill Xue <Bill Xue@DESKTOP-OAS7H28>
2017-01-19 11:13:09 -0800

    k

--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
@@ -35,10 +35,10 @@
             // buttonOne
             // 
             this.buttonOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonOne.Location = new System.Drawing.Point(37, 125);
-            this.buttonOne.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonOne.Location = new System.Drawing.Point(28, 99);
+            this.buttonOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonOne.Name = "buttonOne";
-            this.buttonOne.Size = new System.Drawing.Size(91, 66);
+            this.buttonOne.Size = new System.Drawing.Size(68, 53);
             this.buttonOne.TabIndex = 0;
             this.buttonOne.Text = "1";
             this.buttonOne.UseVisualStyleBackColor = true;
@@ -47,20 +47,19 @@
             // textBox_number
             // 
             this.textBox_number.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_number.Location = new System.Drawing.Point(37, 30);
-            this.textBox_number.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_number.Location = new System.Drawing.Point(28, 24);
             this.textBox_number.Name = "textBox_number";
-            this.textBox_number.Size = new System.Drawing.Size(1080, 56);
+            this.textBox_number.Size = new System.Drawing.Size(811, 44);
             this.textBox_number.TabIndex = 2;
             // 
             // SimpleCalculator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 717);
+            this.ClientSize = new System.Drawing.Size(886, 574);
             this.Controls.Add(this.textBox_number);
             this.Controls.Add(this.buttonOne);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SimpleCalculator";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
