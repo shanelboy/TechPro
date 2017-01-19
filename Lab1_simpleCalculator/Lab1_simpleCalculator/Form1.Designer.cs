28170f06302fe58298d3e0cd3cdd93524b2302bd
Bill Xue <Bill Xue@DESKTOP-OAS7H28>
2017-01-19 10:59:41 -0800

    wat

--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
@@ -30,17 +30,16 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // SimpleCalculator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 440);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(880, 666);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "SimpleCalculator";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
