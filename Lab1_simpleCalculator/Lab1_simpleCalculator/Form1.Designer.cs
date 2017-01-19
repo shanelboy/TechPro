66d520e992c45698beedc9328b230a912ae578de
Bill Xue <Bill Xue@DESKTOP-OAS7H28>
2017-01-19 10:40:19 -0800

    fdg

--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab1_simpleCalculator
 {
-    partial class Form1
+    partial class SimpleCalculator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,22 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox_Number = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // textBox_Number
+            // 
+            this.textBox_Number.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Number.Location = new System.Drawing.Point(39, 23);
+            this.textBox_Number.Name = "textBox_Number";
+            this.textBox_Number.Size = new System.Drawing.Size(478, 44);
+            this.textBox_Number.TabIndex = 0;
+            // 
+            // SimpleCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(545, 440);
+            this.ClientSize = new System.Drawing.Size(547, 620);
+            this.Controls.Add(this.textBox_Number);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "SimpleCalculator";
+            this.Text = "SimpleCalculator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_Number;
     }
 }
 
--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Lab1_simpleCalculator
 {
-    public partial class Form1 : Form
+    public partial class SimpleCalculator : Form
     {
-        public Form1()
+        public SimpleCalculator()
         {
             InitializeComponent();
         }
