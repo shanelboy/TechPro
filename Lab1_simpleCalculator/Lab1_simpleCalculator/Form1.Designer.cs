eba44c8b8457ce6b78cbe8bd0e371a0861192d5b
Bill Xue <Bill Xue@DESKTOP-OAS7H28>
2017-01-19 11:11:56 -0800

    k

--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.buttonOne = new System.Windows.Forms.Button();
             this.textBox_number = new System.Windows.Forms.TextBox();
+            this.buttonThree = new System.Windows.Forms.Button();
+            this.buttonTwo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonOne
             // 
             this.buttonOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonOne.Location = new System.Drawing.Point(28, 100);
+            this.buttonOne.Location = new System.Drawing.Point(28, 342);
             this.buttonOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonOne.Name = "buttonOne";
-            this.buttonOne.Size = new System.Drawing.Size(108, 53);
+            this.buttonOne.Size = new System.Drawing.Size(63, 53);
             this.buttonOne.TabIndex = 0;
             this.buttonOne.Text = "1";
             this.buttonOne.UseVisualStyleBackColor = true;
@@ -51,16 +53,40 @@
             this.textBox_number.Size = new System.Drawing.Size(441, 44);
             this.textBox_number.TabIndex = 2;
             // 
+            // buttonThree
+            // 
+            this.buttonThree.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonThree.Location = new System.Drawing.Point(199, 342);
+            this.buttonThree.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonThree.Name = "buttonThree";
+            this.buttonThree.Size = new System.Drawing.Size(63, 53);
+            this.buttonThree.TabIndex = 3;
+            this.buttonThree.Text = "3";
+            this.buttonThree.UseVisualStyleBackColor = true;
+            // 
+            // buttonTwo
+            // 
+            this.buttonTwo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTwo.Location = new System.Drawing.Point(114, 342);
+            this.buttonTwo.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTwo.Name = "buttonTwo";
+            this.buttonTwo.Size = new System.Drawing.Size(63, 53);
+            this.buttonTwo.TabIndex = 4;
+            this.buttonTwo.Text = "2";
+            this.buttonTwo.UseVisualStyleBackColor = true;
+            // 
             // SimpleCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(557, 637);
+            this.Controls.Add(this.buttonTwo);
+            this.Controls.Add(this.buttonThree);
             this.Controls.Add(this.textBox_number);
             this.Controls.Add(this.buttonOne);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SimpleCalculator";
-            this.Text = "Form1";
+            this.Text = "SimpleCalculator";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -71,6 +97,8 @@
 
         private System.Windows.Forms.Button buttonOne;
         private System.Windows.Forms.TextBox textBox_number;
+        private System.Windows.Forms.Button buttonThree;
+        private System.Windows.Forms.Button buttonTwo;
     }
 }
 
