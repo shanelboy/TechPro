7e2f89e5b1d3609141350d23a443b7f786da4036
Bill Xue <Bill Xue@DESKTOP-OAS7H28>
2017-01-19 11:04:15 -0800

    some change

--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.Designer.cs
@@ -28,37 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonOne = new System.Windows.Forms.Button();
+            this.textBox_number = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonOne
             // 
-            this.button1.Location = new System.Drawing.Point(28, 100);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 18);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOne.Location = new System.Drawing.Point(28, 100);
+            this.buttonOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonOne.Name = "buttonOne";
+            this.buttonOne.Size = new System.Drawing.Size(108, 53);
+            this.buttonOne.TabIndex = 0;
+            this.buttonOne.Text = "1";
+            this.buttonOne.UseVisualStyleBackColor = true;
             // 
-            // checkBox1
+            // textBox_number
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(292, 114);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(113, 24);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.textBox_number.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_number.Location = new System.Drawing.Point(28, 24);
+            this.textBox_number.Name = "textBox_number";
+            this.textBox_number.Size = new System.Drawing.Size(441, 44);
+            this.textBox_number.TabIndex = 2;
             // 
             // SimpleCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 352);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(557, 637);
+            this.Controls.Add(this.textBox_number);
+            this.Controls.Add(this.buttonOne);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "SimpleCalculator";
             this.Text = "Form1";
@@ -70,8 +69,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonOne;
+        private System.Windows.Forms.TextBox textBox_number;
     }
 }
 
