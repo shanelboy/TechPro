1640cda84a3de10697da13d559f1cd4cc9e4a817
Sean <Sean@DESKTOP-3HR3C52>
2017-01-23 23:17:55 -0800

    as

--- Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.cs
+++ Lab1_simpleCalculator/Lab1_simpleCalculator/Form1.cs
@@ -115,6 +115,12 @@ namespace Lab1_simpleCalculator
             }
         }
 
+        /// <summary>
+        /// Displays the button pressed onto the textbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
         private void Calculator_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == '0')
@@ -199,6 +205,11 @@ namespace Lab1_simpleCalculator
 
         }
 
+        /// <summary>
+        /// Turns the calculator on and off
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonOn_Click(object sender, EventArgs e)
         {
             if (buttonOn.Text == "ON")
@@ -219,61 +230,111 @@ namespace Lab1_simpleCalculator
             }
         }
 
+        /// <summary>
+        /// button for number 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonOne_Click(object sender, EventArgs e)
         {
             SendKeys.Send("1");
         }
-
+        /// <summary>
+        /// button for number 2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonTwo_Click(object sender, EventArgs e)
         {
             SendKeys.Send("2");
         }
-
+        /// <summary>
+        /// button for number 9
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonNine_Click(object sender, EventArgs e)
         {
             SendKeys.Send("9");
         }
-
+        /// <summary>
+        /// button for number 4
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonFour_Click(object sender, EventArgs e)
         {
             SendKeys.Send("4");
         }
-
+        /// <summary>
+        /// button for number 3
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonThree_Click(object sender, EventArgs e)
         {
             SendKeys.Send("3");
         }
-
+        /// <summary>
+        /// button for number 8
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonEight_Click(object sender, EventArgs e)
         {
             SendKeys.Send("8");
         }
-
+        /// <summary>
+        /// button for number 5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonFive_Click(object sender, EventArgs e)
         {
             SendKeys.Send("5");
         }
-
+        /// <summary>
+        /// button for number 7
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonSeven_Click(object sender, EventArgs e)
         {
             SendKeys.Send("7");
         }
-
+        /// <summary>
+        /// button for number 6
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonSix_Click(object sender, EventArgs e)
         {
             SendKeys.Send("6");
         }
-
+        /// <summary>
+        /// button for number 0
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonZero_Click(object sender, EventArgs e)
         {
             SendKeys.Send("0");
         }
-
+        /// <summary>
+        /// button for decimal point
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonDot_Click(object sender, EventArgs e)
         {
             SendKeys.Send(".");
         }
 
+        /// <summary>
+        /// button for the negative and positive sign
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonSign_Click(object sender, EventArgs e)
         {
             if (textBox_number.Text.Substring(0, 1) == "-")
@@ -292,30 +353,50 @@ namespace Lab1_simpleCalculator
                 textBox_memory.Text = "-" + textBox_memory.Text;
             }
         }
-
+        /// <summary>
+        /// button for minus operand
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonMinus_Click(object sender, EventArgs e)
         {
             SendKeys.Send("-");
             checkOperation('-');
         }
-
+        /// <summary>
+        /// button for divide operand
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonDivide_Click(object sender, EventArgs e)
         {
             SendKeys.Send("/");
             checkOperation('/');
         }
-
+        /// <summary>
+        /// button for multiplication operand
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonMulti_Click(object sender, EventArgs e)
         {
             SendKeys.Send("*");
             checkOperation('*');
         }
-
+        /// <summary>
+        /// button for equal operand
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonEquals_Click(object sender, EventArgs e)
         {
             checkOperation('=');
         }
-
+        /// <summary>
+        /// button for backspace
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonBackspace_Click(object sender, EventArgs e)
         {
             if (textBox_number.Text.Length > 0)
@@ -323,18 +404,30 @@ namespace Lab1_simpleCalculator
                 textBox_number.Text = textBox_number.Text.Substring(0, textBox_number.Text.Length - 1);
             }
         }
-
+        /// <summary>
+        /// button for clear sign
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonCE_Click_1(object sender, EventArgs e)
         {
             textBox_number.Text = string.Empty;
         }
-
+        /// <summary>
+        /// button for plus operand
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonPlus_Click_1(object sender, EventArgs e)
         {
             SendKeys.Send("+");
             checkOperation('+');
         }
-
+        /// <summary>
+        /// button for clear all
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonC_Click(object sender, EventArgs e)
         {
             textBox_number.Text = string.Empty;
